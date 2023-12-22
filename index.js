197162ad097786488fbf9799d73447a81bcbbbba
banan <banan.faouri2001@gmail.com>
2023-12-22 16:18:49 +0300

    done

--- index.js
+++ index.js
@@ -1,1 +1,56 @@
 // Write your solution here!
+let cats= ["Milo", "Otis", "Garfield"];
+beforeEach(function () {
+    cats.length = 0;
+  
+    cats.push("Milo", "Otis", "Garfield");
+  });
+
+function destructivelyAppendCat (name) {
+    cats.push(name);
+
+}
+
+function destructivelyPrependCat (name) {
+    cats.unshift(name);
+}
+function destructivelyRemoveLastCat () {
+    cats.pop();
+
+}
+function destructivelyRemoveFirstCat () {
+    cats.shift();
+
+}
+
+function appendCat (name) {
+  const a =[...cats,name]
+
+  return a;
+}
+
+function  prependCat(name){
+    let a =[name , ...cats]
+    return a;
+
+}
+function removeLastCat(){
+    let a= cats.slice(0,cats.length-1)
+    return a;
+}
+function removeFirstCat(){
+   let a = cats.slice(1)
+   return a;
+   
+}
+
+     //destructivelyAppendCat("Ralph");
+     destructivelyPrependCat("Bob");
+    // destructivelyRemoveLastCat();
+    // destructivelyRemoveFirstCat ();
+    appendCat("Broom");
+   // removeFirstCat()
+   
+    
+               
+
